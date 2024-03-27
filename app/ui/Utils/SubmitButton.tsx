8ce7820368b491ce43fc17e5e6fc05e5bf3911cc
dzzirima <dzzirima@gmail.com>
2024-03-27 12:23:08 +0200

    feat: submit button now working

--- app/ui/Utils/SubmitButton.tsx
+++ app/ui/Utils/SubmitButton.tsx
@@ -12,7 +12,7 @@ export default function SubmitButton(passedData:props) {
     const { pending } = useFormStatus();
     const {label} = passedData
     return (
-      <Button variant="contained"  type = "submit" className="mt-4 w-full bg-black" aria-disabled={pending}>
+      <Button variant="contained"  type = "submit" className="mt-4 w-full bg-black hover:bg-black" aria-disabled={pending}>
         {`${label}`} { pending ?(<CircularProgress    className="ml-auto h-5 w-5" size={15} color="success"/>):(<CheckCircleOutlineIcon className="ml-auto h-5 w-5 " />)} 
       </Button>
     );
